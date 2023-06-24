607eefa0539813f3e5093ba1ae627acf63286426
Bryson Tanner <bltanner105@gmail.com>
2023-06-23 21:53:28 -0500

    feat: Add stubs of more dots to test engine

---
+++ BattleEngine/BattlingDots.cpp
@@ -0,0 +1,48 @@
+#include "BattlingDots.h"
+
+#include <random>
+
+namespace battle::test {
+  std::string Test1::getName() const noexcept {
+    return "Random";
+  }
+  void Test1::wasEliminated(std::uint32_t team, std::uint32_t killer, std::uint32_t row, std::uint32_t column) noexcept {
+    return;
+  }
+  battle::RunAction Test1::run(std::uint32_t team, std::array<std::uint32_t, 8> view, std::uint32_t row, std::uint32_t column) noexcept {
+    static std::random_device rd;
+    static std::mt19937 generator{rd()};
+    static std::uniform_int_distribution actDist(0, 3);
+    static std::uniform_int_distribution dirDist(0, 7);
+
+    battle::RunAction::Type action = static_cast<::battle::RunAction::Type>(actDist(generator));
+    battle::Direction direction = static_cast<::battle::Direction>(dirDist(generator));
+    switch (action) {
+      case ::battle::RunAction::WAIT:
+        return ::battle::RunAction{.type = action};
+      case ::battle::RunAction::MOVE:
+        return ::battle::RunAction{.type = action,
+                                   .direction = direction};
+      default:
+        return ::battle::RunAction{.type = ::battle::RunAction::WAIT};
+    }
+  }
+
+  std::string Test2::getName() const noexcept {
+    return "Multiply";
+  }
+  void Test2::wasEliminated(std::uint32_t team, std::uint32_t killer, std::uint32_t row, std::uint32_t column) noexcept {
+  }
+  battle::RunAction Test2::run(std::uint32_t team, std::array<std::uint32_t, 8> view, std::uint32_t row, std::uint32_t column) noexcept {
+    return battle::RunAction();
+  }
+
+  std::string Test3::getName() const noexcept {
+    return "Always Attack";
+  }
+  void Test3::wasEliminated(std::uint32_t team, std::uint32_t killer, std::uint32_t row, std::uint32_t column) noexcept {
+  }
+  battle::RunAction Test3::run(std::uint32_t team, std::array<std::uint32_t, 8> view, std::uint32_t row, std::uint32_t column) noexcept {
+    return battle::RunAction();
+  }
+}  // namespace battle::test
--- BattleEngine/BattlingDots.h
+++ BattleEngine/BattlingDots.h
@@ -1,1 +1,38 @@
+#ifndef DOTS_BATTLE_BATTLE_ENGINE_BATTLING_DOTS_H
+#define DOTS_BATTLE_BATTLE_ENGINE_BATTLING_DOTS_H
 
+#include <Dot.h>
+
+namespace battle::test {
+  class Test1 : public Dot {
+   public:
+    std::string getName() const noexcept override;
+    void wasEliminated(std::uint32_t team, std::uint32_t killer,
+                       std::uint32_t row, std::uint32_t column) noexcept override;
+
+    ::battle::RunAction run(std::uint32_t team, std::array<std::uint32_t, 8> view,
+                            std::uint32_t row, std::uint32_t column) noexcept override;
+  };
+
+  class Test2 : public Dot {
+   public:
+    std::string getName() const noexcept override;
+    void wasEliminated(std::uint32_t team, std::uint32_t killer,
+                       std::uint32_t row, std::uint32_t column) noexcept override;
+
+    ::battle::RunAction run(std::uint32_t team, std::array<std::uint32_t, 8> view,
+                            std::uint32_t row, std::uint32_t column) noexcept override;
+  };
+
+  class Test3 : public Dot {
+   public:
+    std::string getName() const noexcept override;
+    void wasEliminated(std::uint32_t team, std::uint32_t killer,
+                       std::uint32_t row, std::uint32_t column) noexcept override;
+
+    ::battle::RunAction run(std::uint32_t team, std::array<std::uint32_t, 8> view,
+                            std::uint32_t row, std::uint32_t column) noexcept override;
+  };
+}  // namespace battle::test
+
+#endif
